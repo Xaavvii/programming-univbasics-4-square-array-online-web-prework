0fb601e4752693f7f9032493c7a12ce57aef5271
github username <github email address>
2019-07-26 19:35:27 -0400

    Done.

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,3 +1,14 @@
 def square_array(array)
   # your code here
+  counter = 0;
+  square_array = [];
+
+  while counter < array.length
+
+    square_array.push array[counter] * array[counter];
+    counter += 1;
+  end
+
+  return square_array;
+
 end
